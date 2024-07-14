28d1e4420d9cdef57b4af1d6fb66c4871ceb01de
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-07-14 15:21:33 +0900

    3주차 과제 업로드

--- chanyoung/week3/src/HandButton.js
+++ chanyoung/week3/src/HandButton.js
@@ -1,6 +1,5 @@
 import HandIcon from './HandIcon';
 
-//아이콘이 그려진 버튼을 클릭하면 콘솔에 각각 rock, scissor, paper 가 출력되는 버튼
 function HandButton({ value, onClick }) {
   const handleClick = () => onClick(value);
   return (
--- chanyoung/week3/src/HandIcon.js
+++ chanyoung/week3/src/HandIcon.js
@@ -2,14 +2,12 @@ import rockImg from './assets/rock.svg';
 import scissorImg from './assets/scissor.svg';
 import paperImg from './assets/paper.svg';
 
-//객체 생성
 const IMAGES = {
   rock: rockImg,
   scissor: scissorImg,
   paper: paperImg,
 };
 
-//Prop 의 값에 따라 가위, 바위, 보 이미지를 보여주기
 function HandIcon({ value }) {
   const src = IMAGES[value];
   return <img src={src} alt={value} />;
