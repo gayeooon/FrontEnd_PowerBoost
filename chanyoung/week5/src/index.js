676f5a762d01b0e31f11c59fbcd652c17ed4cd01
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-07-26 16:04:57 +0900

    5주차 과제 업로드

--- chanyoung/week5/src/index.js
+++ chanyoung/week5/src/index.js
@@ -1,5 +1,10 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom';
 import App from './components/App';
+import { LocaleProvider } from './contexts/LocaleContext';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App/>);
+ReactDOM.render(
+  <LocaleProvider defaultValue="ko">
+    <App />
+  </LocaleProvider>,
+  document.getElementById('root')
+);
