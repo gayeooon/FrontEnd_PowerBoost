35312e5e0ee4d70b5c6687605dd93cd2b3901595
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-08-11 16:24:24 +0900

    7주차 과제 업로드

--- chanyoung/week7/src/Main.js
+++ chanyoung/week7/src/Main.js
@@ -1,28 +1,31 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./components/App";
-import HomePage from "./pages/HomePage";
-import CoursePage from "./pages/CoursePage";
-import CourseListPage from "./pages/CourseListPage";
-import QuestionListPage from "./pages/QuestionListPage";
-import QuestionPage from "./pages/QuestionPage";
-import WishlistPage from "./pages/WishlistPage";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './components/App';
+import HomePage from './pages/HomePage';
+import CoursePage from './pages/CoursePage';
+import CourseListPage from './pages/CourseListPage';
+import QuestionPage from './pages/QuestionPage';
+import QuestionListPage from './pages/QuestionListPage';
+import WishlistPage from './pages/WishlistPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function Main() {
   return (
     <BrowserRouter>
-      <App>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="courses" element={<CourseListPage />} />
-          <Route
-            path="courses/react-frontend-development"
-            element={<CoursePage />}
-          />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="courses">
+            <Route index element={<CourseListPage />} />
+            <Route path=":courseSlug" element={<CoursePage />} />
+          </Route>
+          <Route path="questions">
+            <Route index element={<QuestionListPage />} />
+            <Route path=":questionId" element={<QuestionPage />} />
+          </Route>
           <Route path="wishlist" element={<WishlistPage />} />
-          <Route path="questions" element={<QuestionListPage />} />
-          <Route path="questions/616825" element={<QuestionPage />} />
-        </Routes>
-      </App>
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
